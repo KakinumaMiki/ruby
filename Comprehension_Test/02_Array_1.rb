235cfcdc9977ad362daa2aa4283a016a4499dad5
mikity <78365241+KakinumaMiki@users.noreply.github.com>
2021-03-19 14:33:07 +0900

    Ruby Climbing 理解度確認テスト Ruby編終了

---
+++ Comprehension_Test/02_Array_1.rb
@@ -0,0 +1,30 @@
+# 2. 配列を操作してみよう1
+
+nums = [1, 3, 5, 8, 4]
+
+# - 2-1. イテレータを使用し、配列内の数値の合計値を出力してみよう。
+puts '2-1 配列内の数値の合計'
+sum = 0
+nums.each do |num|
+  sum += num
+end
+puts sum
+
+# - 2-2. イテレータを使用し、配列内から偶数の数値のみを出力してみよう。
+puts '2-2 偶数'
+nums.each do |num|
+  if num % 2 == 0
+    puts num
+  end
+end
+
+# - 2-3. イテレータを使用し、4以下の値の合計値を出力してみよう。
+puts '2-3 4以下の合計'
+even_sum = 0
+nums.each do |num|
+  if num <= 4
+    # puts "num: #{num}"
+    even_sum += num
+  end
+end
+puts even_sum
---
+++ Comprehension_Test/03_Array_2.rb
@@ -0,0 +1,26 @@
+# 3. 配列を操作してみよう2
+
+ dev_lang = ['Ruby', 'Java', 'PHP', 'C', 'C#']
+
+# - 3-1. イテレータを使用し、文字数が3文字以上の情報だけを出力してみよう。
+puts '3-1 文字数が3文字以上'
+dev_lang.each do |s|
+  if s.size >= 3
+    puts s
+  end
+end
+
+# - 3-2. イテレータを使用し、RubyとPHPのみを出力してみよう。
+puts '3-2 RubyとPHPのみ出力'
+dev_lang.each do |s|
+  if s == 'Ruby' or s == 'PHP'
+    puts s
+  end
+end
+
+# - 3-3. メソッドを使用し配列に’JavaScript’という文字列を追加した後、イテレータで配列内の文字列を全て出力してみよう。
+puts '3-3 文字追加'
+dev_lang.push('JavaScript')
+dev_lang.each do |s|
+  puts s
+end
